bec7595d3be4237ed71765a474596c2404c46e86
Rupali Shewale <rupalishewale2005@gmail.com>
2025-10-20 20:14:24 +0530

    added solution 2

--- Arrays&Strings/Day5/Problem13.py
+++ Arrays&Strings/Day5/Problem13.py
@@ -1,10 +1,13 @@
-# Reverse a string in-place
+# Reverse a string 
+# Enter a string: Hello World         
+# Reversed string: dlroW olleH
 
-def reverse_string(str):
-    chars = list(str)
+#solution 1: using 2 pointer
+def reverse_string(s):
+    chars = list(s)
     
     left = 0
-    right = len(str) - 1 
+    right = len(s) - 1 
 
     while left < right:
         chars[left], chars[right] = chars[right], chars[left]
@@ -13,5 +16,15 @@ def reverse_string(str):
 
     return ''.join(chars)
 
+#solution 2: using another string
+def reverse_string2(s):
+    reversed_s = ""
+    for ch in s:
+        reversed_s = ch + reversed_s
+    
+    return reversed_s
+
+
 str = input("Enter a string: ")
 print("Reversed string:", reverse_string(str))
+print("Reversed string:", reverse_string2(str))
